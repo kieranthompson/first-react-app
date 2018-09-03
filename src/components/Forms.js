c2b0c056e5a5767f1fe461dffe474a1a0081765c
kieran thompson <kieranjsthompson@gmail.com>
2018-09-03 17:03:52 +0100

    updated class components to stateless functional components

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,16 +1,11 @@
 import React from "react";
 
-class Forms extends React.Component {
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.props.getWeather}>
-                    <input type='text' name='city' placeholder='City...'/>
-                    <input type='text' name='country' placeholder='Country...'/>
-                    <input type='submit' value='Get Weather' />
-                </form>
-            </div>
-        )
-    }
-}
-export default Forms;
+const Form = props => ( 
+    
+    <form onSubmit={ props.getWeather }>
+        <input type='text' name='city' placeholder='City...'/>
+        <input type='text' name='country' placeholder='Country...'/>
+        <input type='submit' value='Get Weather' />
+    </form>
+)
+export default Form;
