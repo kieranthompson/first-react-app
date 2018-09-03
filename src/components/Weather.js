0ecf590bbada6dcdc3c5a55ef300509980bab479
kieran thompson <kieranjsthompson@gmail.com>
2018-09-03 17:05:33 +0100

    removed this keyword from props.country

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Weather = props => ( 
     <div>
-        { props.city && this.props.country && <p> Location: { props.city }, { props.country } </p> }
+        { props.city && props.country && <p> Location: { props.city }, { props.country } </p> }
         { props.temperature && <p> Temperature: { props.temperature } Celsius</p> }
         { props.humidity && <p> Humidity:  { props.humidity } </p> }
         { props.description && <p> Weather Description: { props.description } </p> }
